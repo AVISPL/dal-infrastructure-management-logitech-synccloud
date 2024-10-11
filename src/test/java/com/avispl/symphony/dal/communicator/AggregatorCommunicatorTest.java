3d5ea5150e0dbdb7a5ea39b2d3f0301491091845
maksym.rossiitsev <maksym.rossiitsev@avispl.com>
2024-10-11 16:40:21 +0300

    SYAL-1871 Logi Sync Cloud (Beta) DAL aggregator

--- src/test/java/com/avispl/symphony/dal/communicator/AggregatorCommunicatorTest.java
+++ src/test/java/com/avispl/symphony/dal/communicator/AggregatorCommunicatorTest.java
@@ -31,52 +31,9 @@ public class AggregatorCommunicatorTest {
         communicator.setPort(443);
 
         communicator.setApiCertificate("-----BEGIN CERTIFICATE-----\n" +
-                "MIIDGzCCAgMCFGGJLZHUZaQ3cnlAXsk6MTni9nJ3MA0GCSqGSIb3DQEBCwUAMBsx\n" +
-                "GTAXBgNVBAMMEHB1YmxpYy1hcGktY2EtdjEwHhcNMjQwOTMwMTE1NDA3WhcNMjcw\n" +
-                "OTMwMTE1NDA3WjB5MSkwJwYDVQQKDCBOU1hJM3VHV2ZnVFpkRjRXa3hJb083a2lr\n" +
-                "cDJFczU1ZzExMC8GA1UECwwoUDBoVE52aU9TUzJuSnBtRTduUEcwMnM2a3JMUGVk\n" +
-                "Wkw2Z0FDbDc0VDEZMBcGA1UEAwwQR1g5cnBKOThzekk0ZTB5MzCCASIwDQYJKoZI\n" +
-                "hvcNAQEBBQADggEPADCCAQoCggEBAJcy2nzkDoR3pte4NnjCIhBI6GzEGPX0yDki\n" +
-                "yYgcJL8Ve8r4PJNssRkBmXrHL9OsBjkxjkoBcGFoO9nM+h8+7c4Jv1PTfyLH3JQ1\n" +
-                "19FgLff7EL6T6/6epayeWQHh2ycvjBZt8sB731B2MnLtxTLZHPrxFfYqPYawvWkw\n" +
-                "Jm9Ci7kX2aMQnIgZgRr6NhmbWpqKTX+tVJLzsZaCFrN3wNYvEfBgsDlfsxDqRrZz\n" +
-                "wJNKftV9EOhcW4jIjeQiM32hdmsb+vgrCsf1WRty/tLek6ga5/fYTJzjXriMqUcQ\n" +
-                "P/5ahgizobSahptw4ywl1lt7uSzVy07ta5GjS7cGaCvERQmontkCAwEAATANBgkq\n" +
-                "hkiG9w0BAQsFAAOCAQEAgex3gnKDpLBVUsw0Nbbb9hpeua2qpaJztfNgCwWT6p+n\n" +
-                "oU/2nA7ktPbotJa5FC1bGwBetlCkHureaqZ68TaqNLhY8A1J72Yg8pXDdDcH1/mo\n" +
-                "TOUauphYFwirKTC9yeIrhRco7Q+m/M5pZ0cPkuKf/EbuyksTAhQN58bpnwmsRKxF\n" +
-                "dxIKWTKOACmXJ9I5PEpPmNWdchatkp40eUrw3V/hWTy4XQmcSeAFTBK1OOLn5XcL\n" +
-                "971C0p8QBDa/9qbUr4HqVQA5G0D349HYI8U8fGqNpOOm5YNJZHDVZShe/y68qWfs\n" +
-                "tFt5h0EX9diPfULF3yCFJi1C6Et7cfc7VhpAFudW3w==\n" +
                 "-----END CERTIFICATE-----");
 
         communicator.setApiKey("-----BEGIN PRIVATE KEY-----\n" +
-                "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCXMtp85A6Ed6bX\n" +
-                "uDZ4wiIQSOhsxBj19Mg5IsmIHCS/FXvK+DyTbLEZAZl6xy/TrAY5MY5KAXBhaDvZ\n" +
-                "zPofPu3OCb9T038ix9yUNdfRYC33+xC+k+v+nqWsnlkB4dsnL4wWbfLAe99QdjJy\n" +
-                "7cUy2Rz68RX2Kj2GsL1pMCZvQou5F9mjEJyIGYEa+jYZm1qaik1/rVSS87GWghaz\n" +
-                "d8DWLxHwYLA5X7MQ6ka2c8CTSn7VfRDoXFuIyI3kIjN9oXZrG/r4KwrH9Vkbcv7S\n" +
-                "3pOoGuf32Eyc4164jKlHED/+WoYIs6G0moabcOMsJdZbe7ks1ctO7WuRo0u3Bmgr\n" +
-                "xEUJqJ7ZAgMBAAECggEAAm+WIoigDT/05v53mNweerRZ/j/t2683tz1EEZZ2fGzx\n" +
-                "rh7wBuYA4mjABWuGbeVpwkEgoY5CtmcHU9EgFHcfPgBdMzyx4dMQzth2jiUmXcY5\n" +
-                "36rNagWPS2fPrPPruRwPKItZQQLzNujN6O5uDLgqZdvoISukCx1M0pAtMQIESJU/\n" +
-                "RuF6QNezUkfN582YvnU7ZoOitaKUPScF7lYYBR0ZzM2rlUmTa1Et7WzVbe6dcF+2\n" +
-                "eT1px6yKxobnMMZ5J1OQFrQ0uPVvyUPyR/eu76xLbAvLA6X4W0lO+PxkFW0OpFjH\n" +
-                "I9vETkuugLQntQkkGMcfBfQO/O1ckVWA5IN4XXaKQQKBgQDHPWsxX3RvdhNxDh4b\n" +
-                "VHTE0J0olcaZjjDRRDBeqYjjrUG8ldwzTxa1vmDReAGMZXjO5pu/FDyhYdiEEpz7\n" +
-                "IZQQgUBat2PH8WEAAU6tdwCOSKnUD5zUyQpXkIEZoAxrZkwhztDBn7UMEOTsogtN\n" +
-                "q0zropfolh0VXsruqFldsRGkUQKBgQDCRcgO560SIjldKf+j4emaCoDSfr4oTVsS\n" +
-                "G2vUACsci5WAwx3RFr7GxTb5ltlntheHue4bxHDSGqwxiWgmorRjTsxwLSP+1GJc\n" +
-                "RLMzaRihSi8krI6W4rMIIpv7x57y5NFrIk4dOXvhCOjMqdZNioijmvAOKNIGAbyc\n" +
-                "oDkbaS5YCQKBgFnj2/jqmB2xqBPZyruJ3yTs+frVmESvYlcH7MrIsnoGMSJkenSr\n" +
-                "uhNFUWkwO4KcRWUTpjEPcEtfWdA8lZa4D5ViuyYyl1IeFSVrcZPRnO6U2gpTAO7/\n" +
-                "xSq8h4KIMxJBlRert4OkCornFGGuumrQXmPxd5f4IicCHYyPZ8JdoRgBAoGALi8g\n" +
-                "rEpvXow8TuWZHICsZC5zCZeP5UzehaN3MuMHXLXiSMYZ5Icfu2lO9G7kKD+lwGJ3\n" +
-                "NKqyl4A3x17/H5A2ihVFjLVuhTpAV+cNIv+tF0rngjRzgXNLVHfF1UlThDLZhjqV\n" +
-                "j2UHCixwC5eklrKEYUCIKRWTOFJZYHLGoLmUhuECgYAWWYlfEcMBcHlMdIVEGat2\n" +
-                "75TcJvoSiClUG5D30+LV5RxS+93Em6TkpJgiVUSjpjIuUXdI4WxgJWenQM5+zYhZ\n" +
-                "d1joixKZoiIVAFdszFpL9UHeUXIZAyMwoqryH5sHTcWaeaRIEsgLSQK//HmVkRkC\n" +
-                "UWtvfmPHIzz3IMO06pCktg==\n" +
                 "-----END PRIVATE KEY-----");
 
         communicator.init();
@@ -85,7 +42,7 @@ public class AggregatorCommunicatorTest {
     @Test
     public void testGetMultipleStatistics() throws Exception {
         communicator.getMultipleStatistics();
-        communicator.setOrganizationIds("NSXI3uGWfgTZdF4WkxIoO7kikp2Es55g");
+        communicator.setOrganizationIds("...");
         communicator.retrieveMultipleStatistics();
         Thread.sleep(30000);
         List<Statistics> statistics = communicator.getMultipleStatistics();
@@ -105,7 +62,7 @@ public class AggregatorCommunicatorTest {
 
     @Test
     public void testRetrieveMultipleStatistics() throws Exception {
-        communicator.setOrganizationIds("NSXI3uGWfgTZdF4WkxIoO7kikp2Es55g");
+        communicator.setOrganizationIds("...");
         communicator.setPlaceRetrievalPageSize(15);
         communicator.retrieveMultipleStatistics();
         Thread.sleep(30000);
