165a4f908949eb801df965d90839845a92983e06
maksym.rossiitsev <maksym.rossiitsev@avispl.com>
2025-11-26 17:07:51 +0200

    SYAL-2658 DAL Logi Sync Cloud (Aggregator) - v1 (Integration Story)

--- src/main/java/com/avispl/symphony/dal/communicator/LogiSyncCloudCommunicator.java
+++ src/main/java/com/avispl/symphony/dal/communicator/LogiSyncCloudCommunicator.java
@@ -603,7 +603,13 @@ public class LogiSyncCloudCommunicator extends RestCommunicator implements Aggre
                 List<AggregatedDevice> placeDevices = null;
                 for (JsonNode room : response.at(Constants.URI.FIELD_PLACES)) {
                     Map<String, String> placeProperties = new HashMap<>();
-                    aggregatedDeviceProcessor.applyProperties(placeProperties, room, Constants.MappingModels.PLACE);
+                    placeProperties.put("Place#ID", room.at("/id").asText());
+                    placeProperties.put("Place#Type", room.at("/type").asText());
+                    placeProperties.put("Place#Name", room.at("/name").asText());
+                    placeProperties.put("Place#Group", room.at("/group").asText());
+                    placeProperties.put("Place#Occupancy", room.at("/occupancy").asText());
+                    placeProperties.put("Place#CreatedAt", room.at("/createdAt").asText());
+                    placeProperties.put("Place#SeatCount", room.at("/seatCount").asText());
 
                     placeDevices = aggregatedDeviceProcessor.extractDevices(room);
                     placeDevices.forEach(aggregatedDevice -> {
